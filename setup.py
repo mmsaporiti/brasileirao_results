1e310db4ad2d936b2842a125541c8abde0dfa1a3
Lucas Zimmer <lucaszm@gmail.com>
2022-08-17 10:33:46 -0300

    Arquivo de configuração para criação do .exe

---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+base = None    
+
+executables = [Executable("bolao_brasileiro_player.py", base=base)]
+
+packages = ["idna", "pandas"]
+options = {
+    'build_exe': {    
+        'packages':packages,
+    },    
+}
+
+setup(
+    name = "Marcos Saporiti",
+    options = options,
+    version = "0.1",
+    description = 'Bolão do Brasileirão',
+    executables = executables
+)
